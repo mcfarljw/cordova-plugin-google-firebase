c0e09597f3bb21b9baca7f3b023916b3835f865e
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2018-07-07 21:55:03 -0400

    Added build scripts for testing android and ios

--- build/refresh.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const shell = require('shelljs')
-
-shell.cd('../test')
-shell.exec('cordova plugin remove cordova-plugin-google-firebase')
-shell.exec('cordova plugin add ../')
---
+++ build/test-ios.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const { ensureDirSync, emptyDirSync } = require('fs-extra')
+const { resolve } = require('path')
+const shell = require('shelljs')
+const root = resolve(__dirname, '..')
+
+// ensure cordova directory exists and is empty
+ensureDirSync(`${root}/test`)
+emptyDirSync(`${root}/test`)
+
+// install cordova
+shell.exec(`cordova create ${root}/test com.jernung.testing "Firebase"`)
+shell.cd(`${root}/test`)
+
+// add platforms
+shell.exec('cordova platform add ios')
+
+// add plugins
+shell.exec('cordova plugin add ..')
+
+// force install pods
+shell.cd('platforms/ios')
+shell.exec('pod install')
