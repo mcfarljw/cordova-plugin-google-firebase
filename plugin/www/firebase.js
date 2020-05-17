937f579e73dfbcbeaef0a2107ad12ffc62700ad1
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2020-05-17 11:18:29 -0400

    Cleanup remaining unused admob imports from plugin

--- plugin/www/firebase.js
+++ plugin/www/firebase.js
@@ -1,44 +1,6 @@
 const exec = require('cordova/exec')
 
 module.exports = {
-  admob: {
-    onInterstitialClosed: function (callback) {
-      exec(callback, callback, 'FirebasePlugin', 'onInterstitialClosed', [])
-    },
-    onRewardVideoClosed: function (callback) {
-      exec(callback, callback, 'FirebasePlugin', 'onRewardVideoClosed', [])
-    },
-    onRewardVideoComplete: function (callback) {
-      exec(callback, callback, 'FirebasePlugin', 'onRewardVideoComplete', [])
-    },
-    setup: function (appId, options) {
-      options = options || {}
-
-      options.interstitialId = options.interstitialId || 'ca-app-pub-3940256099942544/1033173712'
-      options.rewardVideoId = options.rewardVideoId || 'ca-app-pub-3940256099942544/5224354917'
-      options.testDevices = options.testDevices || []
-
-      return new Promise(
-        function (resolve, reject) {
-          exec(resolve, reject, 'FirebasePlugin', 'admobSetup', [appId, options.interstitialId, options.rewardVideoId, options.testDevices])
-        }
-      )
-    },
-    showInterstitial: function () {
-      return new Promise(
-        function (resolve, reject) {
-          exec(resolve, reject, 'FirebasePlugin', 'admobShowInterstitial', [])
-        }
-      )
-    },
-    showRewardVideo: function () {
-      return new Promise(
-        function (resolve, reject) {
-          exec(resolve, reject, 'FirebasePlugin', 'admobShowRewardVideo', [])
-        }
-      )
-    }
-  },
   analytics: {
     logEvent: function (name, params) {
       return new Promise(
--- test/www/js/index.js
+++ test/www/js/index.js
@@ -9,39 +9,39 @@ var app = {
   },
   onDeviceReady: function() {
     if (window.plugins) {
-      if (window.device.platform === 'Android') {
-        window.plugins.firebase.admob.setup('ca-app-pub-3940256099942544~3347511713', {
-          interstitialId: 'ca-app-pub-3940256099942544/1033173712',
-          rewardVideoId: 'ca-app-pub-3940256099942544/5224354917'
-        })
-      }
+      // if (window.device.platform === 'Android') {
+      //   window.plugins.firebase.admob.setup('ca-app-pub-3940256099942544~3347511713', {
+      //     interstitialId: 'ca-app-pub-3940256099942544/1033173712',
+      //     rewardVideoId: 'ca-app-pub-3940256099942544/5224354917'
+      //   })
+      // }
 
-      if (window.device.platform === 'iOS') {
-        window.plugins.firebase.admob.setup('ca-app-pub-3940256099942544~1458002511', {
-          interstitialId: 'ca-app-pub-3940256099942544/4411468910',
-          rewardVideoId: 'ca-app-pub-3940256099942544/1712485313'
-        })
-      }
+      // if (window.device.platform === 'iOS') {
+      //   window.plugins.firebase.admob.setup('ca-app-pub-3940256099942544~1458002511', {
+      //     interstitialId: 'ca-app-pub-3940256099942544/4411468910',
+      //     rewardVideoId: 'ca-app-pub-3940256099942544/1712485313'
+      //   })
+      // }
 
-      $('#show-interstitial').click(function () {
-        window.plugins.firebase.admob.showInterstitial()
-      })
+      // $('#show-interstitial').click(function () {
+      //   window.plugins.firebase.admob.showInterstitial()
+      // })
 
-      $('#show-rewarded-video').click(function () {
-        window.plugins.firebase.admob.showRewardVideo()
-      })
+      // $('#show-rewarded-video').click(function () {
+      //   window.plugins.firebase.admob.showRewardVideo()
+      // })
 
-      window.plugins.firebase.admob.onInterstitialClosed(function (callback) {
-        alert('EVENT: interstitial closed')
-      })
+      // window.plugins.firebase.admob.onInterstitialClosed(function (callback) {
+      //   alert('EVENT: interstitial closed')
+      // })
 
-      window.plugins.firebase.admob.onRewardVideoClosed(function (callback) {
-        alert('EVENT: reward video closed')
-      })
+      // window.plugins.firebase.admob.onRewardVideoClosed(function (callback) {
+      //   alert('EVENT: reward video closed')
+      // })
 
-      window.plugins.firebase.admob.onRewardVideoComplete(function (callback) {
-        alert('EVENT: reward video complete')
-      })
+      // window.plugins.firebase.admob.onRewardVideoComplete(function (callback) {
+      //   alert('EVENT: reward video complete')
+      // })
 
       window.plugins.firebase.remoteConfig.setup().then(function () {
         window.plugins.firebase.remoteConfig.getArray('TestArray').then(function (value) {
