1e4dfb0e89154e16fec940ca388b56b39fb4e897
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2020-05-17 10:48:34 -0400

    Remove all admob support for iOS

--- plugin/src/ios/AppDelegate+FirebasePlugin.m
+++ plugin/src/ios/AppDelegate+FirebasePlugin.m
@@ -17,8 +17,6 @@
 
     [FIRApp configure];
 
-    [[GADMobileAds sharedInstance] startWithCompletionHandler:nil];
-
     return YES;
 }
 
--- plugin/src/ios/FirebasePlugin.h
+++ plugin/src/ios/FirebasePlugin.h
@@ -3,27 +3,12 @@
 
 @interface FirebasePlugin : CDVPlugin
 
-@property(nonatomic, strong) NSString *applicationId;
-@property(nonatomic, strong) GADInterstitial *interstitial;
-@property(nonatomic, strong) NSString *interstitialId;
-@property(nonatomic, strong) NSString *interstitialClosedCallbackId;
 @property(nonatomic, strong) FIRRemoteConfig *remoteConfig;
-@property(nonatomic, strong) GADRewardedAd *rewardVideo;
-@property(nonatomic, strong) NSString *rewardVideoClosedCallbackId;
-@property(nonatomic, strong) NSString *rewardVideoCompleteCallbackId;
-@property(nonatomic, strong) NSString *rewardVideoId;
-@property(nonatomic, strong) NSMutableArray *testDevices;
 
-- (void)admobSetup:(CDVInvokedUrlCommand *)command;
-- (void)admobShowInterstitial:(CDVInvokedUrlCommand *)command;
-- (void)admobShowRewardVideo:(CDVInvokedUrlCommand *)command;
 - (void)analyticsLogEvent:(CDVInvokedUrlCommand *)command;
 - (void)analyticsSetScreenName:(CDVInvokedUrlCommand *)command;
 - (void)analyticsSetUserId:(CDVInvokedUrlCommand *)command;
 - (void)analyticsSetUserProperty:(CDVInvokedUrlCommand *)command;
-- (void)onInterstitialClosed:(CDVInvokedUrlCommand *)command;
-- (void)onRewardVideoClosed:(CDVInvokedUrlCommand *)command;
-- (void)onRewardVideoComplete:(CDVInvokedUrlCommand *)command;
 - (void)remoteConfigFetch:(CDVInvokedUrlCommand *)command;
 - (void)remoteConfigSetup:(CDVInvokedUrlCommand *)command;
 - (void)remoteConfigGetArray:(CDVInvokedUrlCommand *)command;
